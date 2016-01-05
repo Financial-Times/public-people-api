04051c10759f2ab685002b3f559af19bbb28df86
Dan Murphy <dan.murphy@ft.com>
2016-01-05 14:54:51 +0000

    update script to print out version number

--- puppet/scripts/set-module-version-from-tag.sh
+++ puppet/scripts/set-module-version-from-tag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 VERSION=`git describe --abbrev=0 --tags`
-MODULE=`find ../ -name Modulefile`
-sed -e s/^version\ '.*'$/version\ \'${VERSION}\'/ ${MODULE} > Modulefile.tmp && mv Modulefile.tmp ${MODULE}
+MODULE=`find . -name Modulefile`
+sed -e s/^version\ '.*'$/version\ \'${VERSION}\'/ ${MODULE} > Modulefile.tmp && mv Modulefile.tmp ${MODULE} && echo "Modulefile version set to ${VERSION}"
