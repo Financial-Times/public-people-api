affec18e4745c17dc69461e2225519fb8d2fbee2
Dan Murphy <dan.murphy@ft.com>
2016-01-06 17:07:30 +0000

    rename dir to see if this fixes deplyment issues

---
+++ puppet/ft-public_people_api/manifests/init.pp
@@ -0,0 +1,48 @@
+class public_people_api {
+
+  $binary_name = "public-people-api"
+  $install_dir = "/usr/local/$binary_name"
+  $binary_file = "$install_dir/$binary_name"
+  $log_dir = "/var/log/apps"
+  $neoURL=hiera("neoURL", "http://localhost:7474/db/data")
+  $port=hiera("port", "8080")
+
+  class { 'common_pp_up': }
+
+  user { $binary_name:
+    ensure    => present,
+  }
+
+  file {
+    $install_dir:
+      mode    => "0664",
+      ensure  => directory;
+
+    $binary_file:
+      ensure  => present,
+      source  => "puppet:///modules/$module_name/$binary_name",
+      mode    => "0755",
+      require => File[$install_dir];
+
+    $log_dir:
+      ensure  => directory,
+      mode    => "0664"
+  }
+
+  service { 'public-people-api':
+    ensure => running,
+    enable => true,
+    binary => $binary_file,
+    flags => "--neoURL=$neoURL --port=$port"
+  }
+
+  # exec { 'restart_app':
+  #   command     => "supervisorctl restart $binary_name",
+  #   path        => "/usr/bin:/usr/sbin:/bin",
+  #   subscribe   => [
+  #     File[$binary_file],
+  #     Class["${module_name}::supervisord"]
+  #   ],
+  #   refreshonly => true
+  # }
+}
