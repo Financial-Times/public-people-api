9f11a6b334f2c2d1fff1d541dafa76b9a6f7375d
Dan Murphy <dan.murphy@ft.com>
2016-01-10 09:15:44 +0000

    add logging level, default is INFO

--- app.go
+++ app.go
@@ -4,6 +4,7 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/Financial-Times/go-fthealth/v1a"
 	"github.com/Financial-Times/public-people-api/people"
@@ -14,15 +15,16 @@ import (
 )
 
 func main() {
-	log.Debugf("public-people-api %+v", os.Args)
 	app := cli.App("public-people-api-neo4j", "A public RESTful API for accessing People in neo4j")
 	neoURL := app.StringOpt("neo-url", "http://localhost:7474/db/data", "neo4j endpoint URL")
 	port := app.StringOpt("port", "8080", "Port to listen on")
+	logLevel := app.StringOpt("log-level", "INFO", "Logging level (DEBUG, INFO, WARN, ERROR)")
 
 	app.Action = func() {
+		setLogLevel(strings.ToUpper(*logLevel))
+		log.Infof("public-people-api will listen on port: %s, connecting to: %s", *port, *neoURL)
 		runServer(*neoURL, *port)
 	}
-
 	app.Run(os.Args)
 }
 
@@ -44,7 +46,23 @@ func runServer(neoURL string, port string) {
 	r.HandleFunc("/people/{uuid}", people.GetPerson).Methods("GET")
 
 	if err := http.ListenAndServe(":"+port, handlers.CombinedLoggingHandler(os.Stdout, r)); err != nil {
-		log.Printf("Unable to start server: %v\n", err)
-		panic(err)
+		log.Fatalf("Unable to start server: %v", err)
 	}
+}
+
+func setLogLevel(level string) {
+	switch level {
+	case "DEBUG":
+		log.SetLevel(log.DebugLevel)
+	case "INFO":
+		log.SetLevel(log.InfoLevel)
+	case "WARN":
+		log.SetLevel(log.WarnLevel)
+	case "ERROR":
+		log.SetLevel(log.ErrorLevel)
+	default:
+		log.Errorf("Requested log level %s is not supported, will default to INFO level", level)
+		log.SetLevel(log.InfoLevel)
+	}
+	log.Debugf("Logging level set to %s", level)
 }
