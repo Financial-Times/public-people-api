4e921487397ae08732fd98637950e479bbb86f12
Dan Murphy <dan.murphy@ft.com>
2016-01-12 09:57:02 +0000

    rename dir

---
+++ people/handlers_test.go
@@ -0,0 +1,17 @@
+package people
+
+// import (
+// 	"encoding/json"
+// 	"github.com/stretchr/testify/assert"
+// 	"testing"
+// )
+
+// TestNeoReadStructToPersonMandatoryFields checks that madatory fields are set even if they are empty or nil / null
+// func TestNeoReadStructToPersonMandatoryFields(t *testing.T) {
+// 	assert := assert.New(t)
+// 	expected := `{"id":"http://api.ft.com/things/","apiUrl":"","types":null,"memberships":[]}`
+// 	person := neoReadStructToPerson(neoReadStruct{})
+// 	personJSON, err := json.Marshal(person)
+// 	assert.NoError(err, "Unable to marshal Person to JSON")
+// 	assert.Equal(expected, string(personJSON), "Actual: %s doesn't match Expected: %s", string(personJSON), expected)
+// }
