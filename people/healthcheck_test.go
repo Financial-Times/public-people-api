15def6663ad932f1e988858ee698d57b62b50a58
George Antoniadis <george@noodles.gr>
2018-03-22 12:46:39 +0000

    Fix health tests

--- people/healthcheck_test.go
+++ people/healthcheck_test.go
@@ -4,6 +4,11 @@ import (
 	"errors"
 	"testing"
 
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/gorilla/mux"
+
 	fthealth "github.com/Financial-Times/go-fthealth/v1_1"
 	suite "github.com/stretchr/testify/suite"
 )
@@ -13,13 +18,11 @@ type HealthCheckTestTestSuite struct {
 	config  HealthConfig
 	service *HealthcheckService
 	checker func() (string, error)
+	router  *mux.Router
 }
 
 func (suite *HealthCheckTestTestSuite) SetupTest() {
-
-}
-
-func (suite *HealthCheckTestTestSuite) TestChecker() {
+	suite.router = mux.NewRouter()
 	suite.config = HealthConfig{
 		AppSystemCode:     "appSystemCode",
 		AppName:           "appName",
@@ -33,23 +36,30 @@ func (suite *HealthCheckTestTestSuite) TestChecker() {
 		checked = true
 		return expStatus, expError
 	}
-	suite.service = &HealthcheckService{
-		config: suite.config,
-		Checks: []fthealth.Check{
-			fthealth.Check{
-				BusinessImpact:   "Tests service checker",
-				Name:             "Test healthcheck",
-				PanicGuide:       "https://dewey.in.ft.com/view/system/public-people-api",
-				Severity:         1,
-				TechnicalSummary: "Does nothing",
-				Checker:          suite.checker,
-			},
+	checks := []fthealth.Check{
+		fthealth.Check{
+			BusinessImpact:   "Tests service checker",
+			Name:             "Test healthcheck",
+			PanicGuide:       "https://dewey.in.ft.com/view/system/public-people-api",
+			Severity:         1,
+			TechnicalSummary: "Does nothing",
+			Checker:          suite.checker,
 		},
 	}
+	suite.service = NewHealthCheckService(checks, suite.config)
+	suite.service.RegisterAdminHandlers(suite.router)
 	status, err := suite.service.Checks[0].Checker()
 	suite.Equal(expStatus, status)
 	suite.Equal(expError, err)
 	suite.True(checked)
+}
+
+func (suite *HealthCheckTestTestSuite) TestGtg_Success() {
+	req := newRequest("GET", "/__gtg", "")
+	rec := httptest.NewRecorder()
+	suite.router.ServeHTTP(rec, req)
+
+	suite.Equal(http.StatusServiceUnavailable, rec.Result().StatusCode)
 }
 
 func TestHealthCheckTestSuite(t *testing.T) {
